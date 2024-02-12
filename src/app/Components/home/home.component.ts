f3c791a9a41dc20df9c6973cd2b1754d667e1005
ChimpChonk <sanjit.poudel28@gmail.com>
2024-02-12 14:46:13 +0100

    wip

---
+++ src/app/Components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../footer/footer.component';
+import { PortfolioComponent } from '../portfolio/portfolio.component';
+import { CvComponent } from '../cv/cv.component';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    PortfolioComponent,
+    CvComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
