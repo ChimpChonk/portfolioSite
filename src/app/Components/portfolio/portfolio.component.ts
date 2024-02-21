d5a82e61cdd6b1fabde05855b7af71c00ec976e4
ChimpChonk <sanjit.poudel28@gmail.com>
2024-02-21 14:54:22 +0100

    wip

--- src/app/Components/portfolio/portfolio.component.ts
+++ src/app/Components/portfolio/portfolio.component.ts
@@ -1,49 +1,57 @@
 import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardData } from '../../interface/interface';
 
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent{
-  images: string[] = [
-    'assets/mealPlanner/mealplanner_1.PNG',
-    'assets/mealPlanner/mealplanner_2.PNG',
-    'assets/mealPlanner/mealplanner_3.PNG',
-    'assets/mealPlanner/mealplanner_4.PNG',
-    'assets/mealPlanner/mealplanner_5.PNG',
-    'assets/mealPlanner/mealplanner_6.PNG',
-    'assets/mealPlanner/mealplanner_7.PNG',
-    'assets/mealPlanner/mealplanner_8.PNG'
+  cardData: CardData[] = [
+    {
+      imageSource: 'assets/mealPlanner/mealplanner_logo.PNG',
+      tags: [
+        { text: 'C#', color: 'purple' },
+        { text: 'ASP.NET', color: 'purple' },
+        { text: 'Angular', color: '#c3002f' },
+        { text: 'HTML', color: 'orange' },
+        { text: 'CSS', color: 'blue' },
+        { text: 'TypeScript', color: 'blue' },
+        { text: 'SQL', color: 'green' },
+      ],
+      title: 'Meal Planner',
+      description: 'Mealplanner is a web application that allows users to plan their meals for the week. Users can add, edit, and delete meals from their meal plan. The application also has a feature that generate a shopping list based on the meals they have planned',
+      link: 'https://github.com/ChimpChonk/P3AngularMealPlanner'
+    },
+    {
+      imageSource: 'assets/fake_tinder_logo.PNG',
+      tags: [
+        { text: 'C#', color: 'blue' },
+        { text: '.NET Blazor', color: 'purple' },
+        { text: 'SQL', color: 'green' },
+      ],
+      title: 'Fake Tinder',
+      description: 'Fake Tinder is a web application that was created using Blazor. The application allows users to create a profile, view other profiles, and like or dislike other profiles. The application also has a feature that allows users to view their matches.',
+      link: 'https://github.com/ChimpChonk/FakeTinder'
+    },
+    {
+      imageSource: 'assets/rock_paper_scissors_logo.PNG',
+      tags: [
+        { text: 'C#', color: 'purple' },
+        { text: 'ASP.NET', color: 'purple' },
+        { text: 'HTML', color: 'orange' },
+        { text: 'CSS', color: 'blue' },
+        { text: 'TypeScript', color: 'blue' },
+      ],
+      title: 'Rock Paper Scissors',
+      description: 'Rock Paper Scissors is a web application that allows users to play the classic game of Rock Paper Scissors. The application also has a feature that keeps track of the users win/loss record.',
+      link: ''
+    },
   ];
-  activeIndex: number = 0;
-
-  constructor() {}
-
-  // Define methods for controlling the carousel
-  prevSlide() {
-    if (this.activeIndex > 0) {
-      this.activeIndex--;
-    } else {
-      this.activeIndex = this.images.length - 1;
-    }
-  }
-
-  nextSlide() {
-    if (this.activeIndex < this.images.length - 1) {
-      this.activeIndex++;
-    } else {
-      this.activeIndex = 0;
-    }
-  }
-
-  setActiveSlide(index: number) {
-    this.activeIndex = index;
-  }
-
 }
---
+++ src/app/interface/interface.ts
@@ -0,0 +1,7 @@
+export interface CardData {
+    imageSource: string;
+    tags: { text: string, color: string }[];
+    title: string;
+    description: string;
+    link: string;
+}
