aa2a4c2738aa42f15f96aabe660693f0d3d7fb6b
ChimpChonk <sanjit.poudel28@gmail.com>
2024-02-22 11:01:50 +0100

    test deploy

--- src/app/Components/portfolio/portfolio.component.ts
+++ src/app/Components/portfolio/portfolio.component.ts
@@ -30,28 +30,44 @@ export class PortfolioComponent{
       link: 'https://github.com/ChimpChonk/P3AngularMealPlanner'
     },
     {
-      imageSource: 'assets/fake_tinder_logo.PNG',
+      imageSource: 'assets/cs50-harvard.png',
       tags: [
-        { text: 'C#', color: 'blue' },
-        { text: '.NET Blazor', color: 'purple' },
+        { text: 'C', color: 'purple' },
+        { text: 'Python', color: 'purple' },
+        { text: 'HTML', color: 'orange' },
+        { text: 'CSS', color: 'blue' },
+        { text: 'JavaScript', color: '#f7df1e' },
         { text: 'SQL', color: 'green' },
       ],
-      title: 'Fake Tinder',
-      description: 'Fake Tinder is a web application that was created using Blazor. The application allows users to create a profile, view other profiles, and like or dislike other profiles. The application also has a feature that allows users to view their matches.',
-      link: 'https://github.com/ChimpChonk/FakeTinder'
+      title: 'Harvard CS50',
+      description: 'I am in the middle of completing the Harvard CS50 course. This course is an introduction to computer science and the art of programming. The course is taught in C, but I have been completing the problem sets in C# and ASP.NET. I have been learning a lot about algorithms, data structures, and web development.',
+      link: 'https://github.com/ChimpChonk/CS50'
     },
     {
-      imageSource: 'assets/rock_paper_scissors_logo.PNG',
+      imageSource: 'assets/website.PNG',
+      tags: [
+        { text: 'Angular', color: '#c3002f' },
+        { text: 'HTML', color: 'orange' },
+        { text: 'CSS', color: 'blue' },
+        { text: 'TypeScript', color: 'blue' },
+      ],
+      title: 'ChimpChonk.com',
+      description: 'This is my personal website. I built this website using Angular. I have been using this website as a way to showcase my work and to practice my web development skills.',
+      link: 'https://github.com/ChimpChonk/portfolioSite'
+    },
+    {
+      imageSource: 'assets/github-logo.jpg',
       tags: [
         { text: 'C#', color: 'purple' },
         { text: 'ASP.NET', color: 'purple' },
         { text: 'HTML', color: 'orange' },
         { text: 'CSS', color: 'blue' },
         { text: 'TypeScript', color: 'blue' },
+        { text: 'SQL', color: 'green' },
       ],
-      title: 'Rock Paper Scissors',
-      description: 'Rock Paper Scissors is a web application that allows users to play the classic game of Rock Paper Scissors. The application also has a feature that keeps track of the users win/loss record.',
-      link: ''
-    },
+      title: 'Other Projects',
+      description: 'I have many other projects that I have worked on while learning programming. I have experience with C#, ASP.NET, Angular, HTML, CSS, TypeScript, and SQL. I have experience with web development, desktop applications, and database management. I am always looking for new projects to work on and new technologies to learn.',
+      link: 'https://github.com/ChimpChonk?tab=repositories'
+    }
   ];
 }
